f93a9dbe44d980e90e933472109eb2b921518883
Paul <you@example.com>
2018-07-12 11:29:53 +0800

    Removed redundant dropdown menu creation code

--- app/src/main/java/com/example/liruk/taskorganizer/TaskEditor.java
+++ app/src/main/java/com/example/liruk/taskorganizer/TaskEditor.java
@@ -20,7 +20,7 @@ public class TaskEditor extends AppCompatActivity {
     DatePicker datePicker;
     TimePicker timePicker;
     Spinner onesDuration;
-    Spinner tenDuration;
+    Spinner tensDuration;
     RatingBar ratingBar;
 
     @Override
@@ -28,22 +28,20 @@ public class TaskEditor extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_editor);
 
-        onesDuration = findViewById(R.id.durationTensDigit);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.digits_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        onesDuration.setAdapter(adapter);
 
-        tenDuration = findViewById(R.id.durationOnesDigit);
-        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
-                R.array.digits_array, android.R.layout.simple_spinner_item);
-        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        tenDuration.setAdapter(adapter1);
+        tensDuration = findViewById(R.id.durationTensDigit);
+        tensDuration.setAdapter(adapter);
+
+        onesDuration = findViewById(R.id.durationOnesDigit);
+        onesDuration.setAdapter(adapter);
 
         taskName = findViewById(R.id.name);
         datePicker = findViewById(R.id.date);
         timePicker = findViewById(R.id.time);
-            timePicker.setIs24HourView(true);
+        timePicker.setIs24HourView(true);
         ratingBar = findViewById(R.id.priorityRating);
     }
 
@@ -55,8 +53,8 @@ public class TaskEditor extends AppCompatActivity {
         int month = datePicker.getMonth();
         int year =  datePicker.getYear();
 
-        int duration = Integer.parseInt(onesDuration.getSelectedItem().toString()) * 10 +
-                Integer.parseInt(tenDuration.getSelectedItem().toString());
+        int duration = Integer.parseInt(tensDuration.getSelectedItem().toString()) * 10 +
+                Integer.parseInt(onesDuration.getSelectedItem().toString());
 
         int hour = timePicker.getCurrentHour();
 
