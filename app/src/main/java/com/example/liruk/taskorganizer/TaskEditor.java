1c7ccdc984066314aa0148b12be1381d1b51049c
Paul <you@example.com>
2018-07-12 17:21:20 +0800

    Adding new files for TaskEditor activity

---
+++ app/src/main/java/com/example/liruk/taskorganizer/TaskEditor.java
@@ -0,0 +1,13 @@
+package com.example.liruk.taskorganizer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TaskEditor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_editor);
+    }
+}
